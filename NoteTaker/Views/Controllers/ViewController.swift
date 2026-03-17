64f4ebafe8435844aa239fb0a68c27f33c3a1a1d
Soumik Bhattacharyya <soumik.bhattacharya@nuvedalearning.com>
2026-03-17 16:02:46 +0530

    Initial commit - UIKit project setup

---
+++ NoteTaker/Views/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoteTaker
+//
+//  Created by Soumik Bhattacharyya on 17/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
